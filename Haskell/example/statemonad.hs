bce613e89e02426631476191170ef3d19546f7cf
yunlu <wylswz@icloud.com>
2019-06-17 23:38:50 +1000

    state monad

---
+++ Haskell/example/statemonad.hs
@@ -0,0 +1,17 @@
+import Control.Monad.State
+{-
+    depends on:
+    - libghc-mtl-dev
+-}
+-- Increase Nth element in list by N
+incList :: [Int] -> [Int]
+incList l = fst (runState (incList' l) 1)
+
+incList' :: [Int] -> State Int [Int]
+incList' [] = return []
+incList' (x:xs) = do
+    newTail <- incList' xs
+    n <- get
+    let res = (x+n):newTail
+    put (n + 1)
+    return res
